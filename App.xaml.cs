8fee3ff8b126016e8c3c08d72f547280efc9c293
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-05-02 22:24:33 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Conways_Game_of_Life
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App
+  {
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Conways_Game_of_Life
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
