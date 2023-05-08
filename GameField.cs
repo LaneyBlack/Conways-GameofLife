17614cd5f4c0fadd5926a31e30f773ba42a0a98e
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-05-08 23:15:44 +0200

    Lots of work done. ToDo's: 1)Infinite edges; 2)Speed choice??; 3)Rule grid; 4)No Uid in button

--- GameField.cs
+++ GameField.cs
@@ -1,16 +1,81 @@
-﻿namespace Conways_Game_of_Life
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Conways_Game_of_Life
 {
     public class GameField
     {
-        public static int[,] Field { get; set; }
-        public static int[,] FieldCopy { get; set; }
+        private int[,] FutureField { get; set; }
+        public int[,] Field { get; set; }
+        public int[,] Rules { get; set; }
+
+        public EventHandler<string> RedrawEvent;
 
         public bool IsPaused { get; set; }
 
         public GameField(int fieldWidth, int fieldHeight)
         {
-            Field = new int[fieldHeight,fieldWidth];
-            FieldCopy = new int[fieldHeight,fieldWidth];
+            Field = new int[fieldHeight, fieldWidth]; //ToDo check if indexes are same in Grid and Here
+            FutureField = new int[fieldHeight, fieldWidth];
+            Rules = new int[2, 9];
+            Rules[0, 3] = 1;
+            Rules[1, 2] = 1;
+            Rules[1, 3] = 1;
+            IsPaused = true;
+        }
+
+        public void RiseRedrawEvent()
+        {
+            if (RedrawEvent != null)
+                RedrawEvent.Invoke(this,"");
+        }
+
+        public void LoopCalculations()
+        {
+            while (true)
+                if (!IsPaused)
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    CalcNextState();
+                    stopwatch.Stop();
+                    if (1500 - stopwatch.ElapsedMilliseconds > 0)
+                        Thread.Sleep((int)(1500 - stopwatch.ElapsedMilliseconds));
+                    RiseRedrawEvent();
+                }
+        }
+
+        public void CalcNextState()
+        {
+            try
+            {
+                FutureField = new int[Field.GetLength(0), Field.GetLength(1)];
+                for (int y = 1; y < Field.GetLength(0) - 1; y++) //ToDo make it infinite
+                for (int x = 1; x < Field.GetLength(1) - 1; x++)
+                {
+                    int aliveNeighbours = 0;
+                    for (int i = -1; i <= 1; i++)
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        aliveNeighbours += Field[y + i, x + j];
+                    }
+
+                    aliveNeighbours -= Field[y, x];
+                    if (Rules[0, aliveNeighbours] == 1 && Rules[1, aliveNeighbours] == 1)
+                        FutureField[y, x] = 1;
+                    else if (Rules[0, aliveNeighbours] == 0 && Rules[1, aliveNeighbours] == 0)
+                        FutureField[y, x] = 0;
+                    else
+                        FutureField[y, x] = Field[y, x];
+                }
+
+                Field = FutureField;
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e);
+            }
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace Conways_Game_of_Life
 {
@@ -14,29 +18,57 @@ namespace Conways_Game_of_Life
     public partial class MainWindow
     {
         private static GameField _gameField;
+        private static Thread _gameFieldThread;
 
         public MainWindow()
         {
             InitializeComponent();
-            var fieldWidth = 50; // Start value
+            const int fieldWidth = 50; // Start value
             FieldWidthTextBox.Text = fieldWidth + "";
             var fieldHeight = (int)(fieldWidth / Width * Height); // Count 
             FieldGridInit(fieldWidth, fieldHeight);
             _gameField = new GameField(fieldWidth, fieldHeight);
+            _gameFieldThread = new Thread(delegate() { _gameField.LoopCalculations(); });
+            _gameFieldThread.Start();
+            _gameField.RedrawEvent += FieldGridRedrawEvent;
         }
 
         private static void PointClick(object sender, RoutedEventArgs e)
         {
+            if (!_gameField.IsPaused) return;
             Button pointButton = (Button)sender;
-            pointButton.Opacity = pointButton.Opacity==100 ? 0 : 100;
+            if (pointButton.Opacity == 100)
+            {
+                pointButton.Opacity = 0;
+                //ToDo Without Uid
+                // Grid.GetRow(pointButton)
+                var tmp = pointButton.Uid.Replace("P", "").Split('-');
+                int i = int.Parse(tmp[0]);
+                int j = int.Parse(tmp[1]);
+                _gameField.Field[i, j] = 0;
+            }
+            else
+            {
+                pointButton.Opacity = 100;
+                var tmp = pointButton.Uid.Replace("P", "").Split('-');
+                int i = int.Parse(tmp[0]);
+                int j = int.Parse(tmp[1]);
+                _gameField.Field[i, j] = 1;
+            }
         }
 
         private void Pause(object sender, RoutedEventArgs e)
         {
+            lock (_gameField)
+                _gameField.IsPaused = true;
+            FieldWidthTextBox.IsEnabled = true;
         }
 
         private void Play(object sender, RoutedEventArgs e)
         {
+            FieldWidthTextBox.IsEnabled = false;
+            lock (_gameField)
+                _gameField.IsPaused = false;
         }
 
         [SuppressMessage("ReSharper.DPA", "DPA0001: Memory allocation issues")]
@@ -50,7 +82,8 @@ namespace Conways_Game_of_Life
             else
                 fieldWidth = int.Parse(textBox.Text);
             var fieldHeight = (int)(fieldWidth / Width * Height);
-            _gameField = new GameField(fieldWidth, fieldHeight);
+            if (_gameField != null)
+                _gameField.Field = new int[fieldWidth, fieldHeight];
             FieldGridInit(fieldWidth, fieldHeight);
         }
 
@@ -84,6 +117,28 @@ namespace Conways_Game_of_Life
                     FieldGrid.Children.Add(border);
                 }
             }
+        }
+
+        private void FieldGridRedraw()
+        {
+            for (int y = 0; y < _gameField.Field.GetLength(0); y++)
+            for (int x = 0; x < _gameField.Field.GetLength(1); x++)
+            {
+                FieldGrid.Children
+                    .Cast<Border>()
+                    .First(e => Grid.GetRow(e) == y && Grid.GetColumn(e) == x)
+                    .Child.Opacity = _gameField.Field[y, x] * 100;
+            }
+        }
+
+        private void FieldGridRedrawEvent(object sender, string e)
+        {
+            Dispatcher.BeginInvoke((Action)FieldGridRedraw);
+        }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            _gameFieldThread.Abort();
         }
     }
 }
